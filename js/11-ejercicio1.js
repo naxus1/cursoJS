7adb4b62ac7ebac7b5eb25f6dc88060fc7acbab3
naxus1 <naxus1@gmail.com>
2019-07-25 16:21:28 -0500

    [ADD] while

--- js/11-ejercicio1.js
+++ js/11-ejercicio1.js
@@ -1,17 +1,18 @@
-'use_strict';
+'use_strict'
 
-//Programa que pide 2 numeros y nos dice cual es mayor el menor o si son iguales
-//If umber is not a number or the number is less than 0, input again
+// Programa que pide 2 numeros y nos dice cual es mayor el menor o si son iguales
+// If umber is not a number or the number is less than 0, input again.
 
-while (number1 < 0 && number2 < 0)
+var number1 = parseInt(prompt("Digite numero 1", 0));
+var number2 = parseInt(prompt("Dogite numero 2", 0));
+
+while (number1 < 0 || number2 < 0 ||isNaN(number1) || isNaN(number2))
 {
-    number1 = parseint(prompt("Digite numero 1", 1));
-    number2 = parseint(prompt("Dogite numero 2", 2));
+number1 = parseInt(prompt("Digite numero 1", 0));
+number2 = parseInt(prompt("Digite numero 2", 0));
 }
 
-
-
-if (number1 == number2) 
+if (number1 === number2) 
 {
     alert("Los numeros son iguales");
 }
