087cb15d768a384e85b724f3d79f4117b37db23a
naxus1 <naxus1@gmail.com>
2019-07-25 07:18:36 -0500

    [ADD] Crete point 11

--- js/11-ejercicio1.js
+++ js/11-ejercicio1.js
@@ -1,8 +1,15 @@
 'use_strict';
 
 //Programa que pide 2 numeros y nos dice cual es mayor el menor o si son iguales
-number1 = parseint(prompt("Digite numero 1", 1));
-number2 = parseint(prompt("Dogite numero 2", 2));
+//If umber is not a number or the number is less than 0, input again
+
+while (number1 < 0 && number2 < 0)
+{
+    number1 = parseint(prompt("Digite numero 1", 1));
+    number2 = parseint(prompt("Dogite numero 2", 2));
+}
+
+
 
 if (number1 == number2) 
 {
@@ -11,9 +18,11 @@ if (number1 == number2)
 else if(number1 > number2)
 {
     alert("el numero mayor es:" + number1);
+    alert("el numero menor es:" + number2);
 }
-else
+else if(number1 < number2)
 {
-
+    alert("el numero mayor es:" + number2);
+    alert("el numero menor es:" + number1);
 }
 
